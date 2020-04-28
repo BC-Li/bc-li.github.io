c78aa59eef653d1542a7196a24f83e3d7f0e4289
BC-Li <251156266@qq.com>
2020-04-28 21:24:23 +0800

    Site updated: 2020-04-28 21:24:23

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,7 @@ const ap = new APlayer({
       {
         name: "Lonely Together",
         artist: 'Avicii feat. Rita Ora',
-        url: 'https://bc-li.github.io/songs/Lonely_Together.mp3',
+        url: 'https://win-web-nf01-sycdn.kuwo.cn/f0ed64dadad8187da5a4906ec5c845d1/5ea826e3/resource/n1/76/67/2837383370.mp3',
         cover: 'http://img3.kuwo.cn/star/albumcover/300/74/8/226733318.jpg',
       }
     ]
